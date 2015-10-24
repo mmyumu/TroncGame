ac59faf257c7b5c7af239f5b32aed195df29935c
mmyumu <mmyumu@gmail.com>
2015-10-24 15:32:42 +0200

    Use Android immersive mode (hide navigation bar)

--- android/src/fr/mmyumu/troncgame/android/AndroidLauncher.java
+++ android/src/fr/mmyumu/troncgame/android/AndroidLauncher.java
@@ -13,6 +13,7 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useImmersiveMode = true;
 		DaggerAdapter daggerAdapter = new DaggerAdapter(TroncGame.class);
 		initialize(daggerAdapter, config);
 	}
