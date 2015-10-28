6f508204cd109bc438c5d66e6d439ce1c8974a90
mmyumu <mmyumu@gmail.com>
2015-10-28 22:43:25 +0100

    #2: Create the dummy overworld screen to be created when clicking on start button

---
+++ core/src/fr/mmyumu/troncgame/overworld/OverworldScreen.java
@@ -0,0 +1,16 @@
+package fr.mmyumu.troncgame.overworld;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+/**
+ * Created by mmyumu on 28/10/2015.
+ */
+public class OverworldScreen extends ScreenAdapter {
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void render(float delta) {
+    }
+}
