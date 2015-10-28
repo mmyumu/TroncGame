2aed28a969e051d5079e8b62b558f28c3dcf2288
mmyumu <mmyumu@gmail.com>
2015-10-28 23:17:44 +0100

    #2: Create overworld loading screen

---
+++ core/src/fr/mmyumu/troncgame/overworld/OverworldLoadingScreen.java
@@ -0,0 +1,38 @@
+package fr.mmyumu.troncgame.overworld;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
+
+import javax.inject.Inject;
+
+import fr.mmyumu.troncgame.TroncGame;
+
+/**
+ * Created by mmyumu on 28/10/2015.
+ */
+public class OverworldLoadingScreen extends ScreenAdapter {
+    private static final String TAG = "OverworldLoadingScreen";
+    private TroncGame troncGame;
+    private AssetManager assetManager;
+
+    @Inject
+    public OverworldLoadingScreen(TroncGame troncGame, AssetManager assetManager) {
+        this.troncGame = troncGame;
+        this.assetManager = assetManager;
+    }
+
+    @Override
+    public void show() {
+        Gdx.app.debug(TAG, "Loading overworld");
+    }
+
+    @Override
+    public void render(float delta) {
+        if (assetManager.update()) {
+            troncGame.setScreen(troncGame.getScreenComponent().createOverworldScreen());
+        }
+        Gdx.app.debug(TAG, ".");
+    }
+}
