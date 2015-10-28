e8e1738a0b932f11f6d781adc045c04693638d85
mmyumu <mmyumu@gmail.com>
2015-10-28 23:36:58 +0100

    #3: Clean useless imports

--- core/src/fr/mmyumu/troncgame/menu/main/MainMenuScreen.java
+++ core/src/fr/mmyumu/troncgame/menu/main/MainMenuScreen.java
@@ -2,9 +2,7 @@ package fr.mmyumu.troncgame.menu.main;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.viewport.ScalingViewport;
