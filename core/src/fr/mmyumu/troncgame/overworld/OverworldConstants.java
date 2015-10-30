0bf9d27ed767b0e5dc759d349a626cb4ba6dccb0
mmyumu <mmyumu@gmail.com>
2015-10-30 02:15:24 +0100

    #6: Constants concerning the Overworld only

---
+++ core/src/fr/mmyumu/troncgame/overworld/OverworldConstants.java
@@ -0,0 +1,10 @@
+package fr.mmyumu.troncgame.overworld;
+
+/**
+ * Constants for the Overworld
+ * Created by mmyumu on 30/10/2015.
+ */
+public interface OverworldConstants {
+    int TILE_WIDTH = 60;
+    int TILE_HEIGHT = 60;
+}
