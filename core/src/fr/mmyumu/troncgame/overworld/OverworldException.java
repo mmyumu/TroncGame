60097d9165faebfdf7b57718e159e7178c427b47
mmyumu <mmyumu@gmail.com>
2015-10-30 02:15:59 +0100

    #6: Add the class for overworld exceptions

---
+++ core/src/fr/mmyumu/troncgame/overworld/OverworldException.java
@@ -0,0 +1,23 @@
+package fr.mmyumu.troncgame.overworld;
+
+/**
+ * Exception about the Overworld
+ * Created by mmyumu on 26/09/2015.
+ */
+public class OverworldException extends RuntimeException {
+    public OverworldException() {
+    }
+
+    public OverworldException(String detailMessage) {
+        super(detailMessage);
+    }
+
+    public OverworldException(String detailMessage, Throwable throwable) {
+        super(detailMessage, throwable);
+    }
+
+    public OverworldException(Throwable throwable) {
+        super(throwable);
+    }
+
+}
