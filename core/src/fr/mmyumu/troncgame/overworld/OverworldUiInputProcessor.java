b3d843792109a66e52492d173ddfff1be7cdd469
mmyumu <mmyumu@gmail.com>
2015-11-09 00:55:01 +0100

    #6: Create a dummy Overworld UI Input Processor

---
+++ core/src/fr/mmyumu/troncgame/overworld/OverworldUiInputProcessor.java
@@ -0,0 +1,10 @@
+package fr.mmyumu.troncgame.overworld;
+
+import com.badlogic.gdx.InputAdapter;
+
+/**
+ * Manage the inputs on the UI elements when Overworld is displayed
+ * Created by mmyumu on 07/11/2015.
+ */
+public class OverworldUIInputProcessor extends InputAdapter {
+}
