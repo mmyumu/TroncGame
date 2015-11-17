11bac55d8c488508c6af3854e84734452eef5eac
mmyumu <mmyumu@gmail.com>
2015-11-17 23:45:33 +0100

    #16: Add dummy dialog loading screen

---
+++ core/src/fr/mmyumu/troncgame/dialog/DialogLoadingScreen.java
@@ -0,0 +1,10 @@
+package fr.mmyumu.troncgame.dialog;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+/**
+ * Screen to be displayed when loading Dialog screen
+ * Created by mmyumu on 17/11/2015.
+ */
+public class DialogLoadingScreen extends ScreenAdapter {
+}
