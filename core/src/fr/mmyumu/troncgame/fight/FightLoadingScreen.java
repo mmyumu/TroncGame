fce8550ce4cf3ed07ec73a8438eb0a4ac390d881
mmyumu <mmyumu@gmail.com>
2015-11-17 23:45:14 +0100

    #13: Add dummy fight loading screen

---
+++ core/src/fr/mmyumu/troncgame/fight/FightLoadingScreen.java
@@ -0,0 +1,10 @@
+package fr.mmyumu.troncgame.fight;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+/**
+ * Screen to be displayed when loading the fights
+ * Created by mmyumu on 17/11/2015.
+ */
+public class FightLoadingScreen extends ScreenAdapter {
+}
