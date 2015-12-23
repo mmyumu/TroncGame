cd93f664f17f527fd50b4b4456f03934ad591ba3
mmyumu <mmyumu@gmail.com>
2015-12-23 23:37:20 +0100

    #40: Add stage for UI in Fight Screen

---
+++ core/src/fr/mmyumu/troncgame/fight/ui/FightUIStage.java
@@ -0,0 +1,14 @@
+package fr.mmyumu.troncgame.fight.ui;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+/**
+ * Stage to display the UI of the fight
+ * Created by mmyumu on 14/12/2015.
+ */
+public class FightUIStage extends Stage {
+    public FightUIStage(Viewport viewport) {
+        super(viewport);
+    }
+}
