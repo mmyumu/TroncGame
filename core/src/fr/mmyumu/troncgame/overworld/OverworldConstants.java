3ea8efedcde9f133771b2e94e22bea492551ed42
mmyumu <mmyumu@gmail.com>
2015-12-31 00:30:39 +0100

    #38: Add overworld constants for menu list sizes, menu list background texture and skin assets

--- core/src/fr/mmyumu/troncgame/overworld/OverworldConstants.java
+++ core/src/fr/mmyumu/troncgame/overworld/OverworldConstants.java
@@ -8,11 +8,20 @@ public interface OverworldConstants {
     int TILE_WIDTH = 90;
     int TILE_HEIGHT = 90;
 
+    int MENU_LIST_WIDTH = 240;
+    int MENU_LIST_HEIGHT = 768;
+
     interface MapPath {
         String VILLAGE = "maps/village.tmx";
     }
 
     interface TexturePath {
         String MAIN_CHARACTER = "data/overworld/character.png";
+        String MENU_LIST = "data/overworld/menu_list.png";
+    }
+
+    interface SkinPath {
+        String SKIN_ATLAS = "data/uiskin.atlas";
+        String SKIN_JSON = "data/uiskin.json";
     }
 }
