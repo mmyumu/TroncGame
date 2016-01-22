462a0fda0c270f78ddc5bb02483a92c85a60487d
mmyumu <mmyumu@gmail.com>
2016-01-23 00:09:57 +0100

    Add the different states of the Fight
    The states of the fight are an enum

---
+++ core/src/fr/mmyumu/troncgame/fight/FightState.java
@@ -0,0 +1,9 @@
+package fr.mmyumu.troncgame.fight;
+
+/**
+ * The different possible states of the Fight
+ * Created by mmyumu on 20/01/2016.
+ */
+public enum FightState {
+    NOTHING_SELECTED, CHARACTER_SELECTED, ACTION_SELECTED
+}
