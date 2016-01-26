baa64de400d4aff8b38e98c0edf97a44d323bb85
mmyumu <mmyumu@gmail.com>
2016-01-26 23:38:13 +0100

    Persist movement through fights
    When a fight is over, the position of the character in the overworld is now the same as the position before triggering the fight
    
    Issue #53

--- core/src/fr/mmyumu/troncgame/overworld/game/OverworldCharacterLogic.java
+++ core/src/fr/mmyumu/troncgame/overworld/game/OverworldCharacterLogic.java
@@ -64,10 +64,6 @@ public class OverworldCharacterLogic {
         }
     }
 
-    public void setMoveTarget(GridPoint2 moveTarget) {
-        this.moveTarget = moveTarget;
-    }
-
     public void cameraMoved(float x, float y) {
         if (moveTarget != null) {
             Gdx.app.debug(TAG, "Camera moved x=" + x + " y=" + y);
@@ -86,6 +82,10 @@ public class OverworldCharacterLogic {
 
     public GridPoint2 getMoveTarget() {
         return moveTarget;
+    }
+
+    public void setMoveTarget(GridPoint2 moveTarget) {
+        this.moveTarget = moveTarget;
     }
 
     private void move(float delta) {
