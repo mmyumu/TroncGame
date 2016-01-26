41e828e7a28de21b8acac32347f82ef4cf85963a
mmyumu <mmyumu@gmail.com>
2016-01-26 23:38:13 +0100

    Persist movement through fights
    When a fight is over, the position of the character in the overworld is now the same as the position before triggering the fight
    
    Issue #53

--- core/src/fr/mmyumu/troncgame/overworld/OverworldScreen.java
+++ core/src/fr/mmyumu/troncgame/overworld/OverworldScreen.java
@@ -49,6 +49,13 @@ public class OverworldScreen extends ScreenAdapter {
         this.gameViewport = gameViewport;
 
         initStages();
+
+        screenState = ScreenState.RUNNING;
+
+        OrthographicCamera gameCamera = (OrthographicCamera) gameViewport.getCamera();
+        map = new OverworldMap(OverworldConstants.MapPath.VILLAGE, gameCamera, assetManager);
+
+        mainCharacter = loadMainCharacter();
     }
 
     private void initStages() {
@@ -63,12 +70,6 @@ public class OverworldScreen extends ScreenAdapter {
     @Override
     public void show() {
         Gdx.app.debug(TAG, "Showing Overworld");
-
-        screenState = ScreenState.RUNNING;
-
-        OrthographicCamera gameCamera = (OrthographicCamera) gameViewport.getCamera();
-        map = new OverworldMap(OverworldConstants.MapPath.VILLAGE, gameCamera, assetManager);
-        mainCharacter = loadMainCharacter();
 
         initInputProcessors();
     }
@@ -164,12 +165,8 @@ public class OverworldScreen extends ScreenAdapter {
      * Load the character and add it to the gameStage
      */
     private OverworldCharacter loadMainCharacter() {
-        float centerX = OverworldConstants.TILE_WIDTH * 1.5f;
-        float centerY = OverworldConstants.TILE_HEIGHT * 1.5f;
-
-
         OverworldCharacter overworldCharacter = troncGame.getOverworldComponent().createOverworldCharacter();
-        overworldCharacter.initCenter((int) centerX, (int) centerY);
+//        overworldCharacter.initCenter((int) centerX, (int) centerY);
         overworldCharacter.setObstaclesLayer(map.getObstaclesLayer());
 
         return overworldCharacter;
